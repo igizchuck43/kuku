595057f5f04ab683fbf03e28b74ae1338a24302c
Igiz <igizstarkle@gmail.com>
2024-08-01 22:47:25 +0300

    Address-Screen

---
+++ lib/screens/address_screen/address_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+import '../../utils/screen_utils.dart';
+import '../../widgets/back_button_ls.dart';
+
+class AddressScreen extends StatelessWidget {
+  static const routeName = '/address_screen';
+
+  const AddressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtils().init(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            BackButtonLS(),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(16),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Add Current Location',
+                          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                          onTap: () {
+                            // Navigator.of(context).pushNamed(MapScreen.routeName);
+                          },
+                          child: Image.asset('assets/images/map.png'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: getProportionateScreenHeight(10),
+                    ),
+                    Row(
+                      children: [
+                        FittedBox(
+                          child: Text(
+                            'Enter your current location and address',
+                            style: TextStyle(
+                              color: kTextColorAccent,
+                              fontSize: getProportionateScreenWidth(16),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: getProportionateScreenHeight(10),
+                    ),
+
+                    // SearchBar(),
+
+                    SizedBox(
+                      height: getProportionateScreenHeight(10),
+                    ),
+                    Divider(),
+                    SizedBox(
+                      height: getProportionateScreenHeight(10),
+                    ),
+                    Expanded(
+                      child: ListTile(
+                        leading: Container(
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                getProportionateScreenWidth(8),
+                              ),
+                            ),
+                            color: kPrimaryGreen,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(
+                              getProportionateScreenWidth(8),
+                            ),
+                            child: Icon(
+                              Icons.location_on_outlined,
+                              size: getProportionateScreenWidth(28),
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        title: Text(
+                          'Current Location',
+                          style: TextStyle(
+                            fontSize: getProportionateScreenWidth(20),
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Queen Elizabeth 89\'8',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/back_button_ls.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../utils/screen_utils.dart';
+
+class BackButtonLS extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtils().init(context);
+    return RawMaterialButton(
+      child: Icon(
+        Icons.arrow_back_ios,
+        color: kPrimaryGreen,
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+      constraints: BoxConstraints.tightFor(
+        width: getProportionateScreenWidth(32),
+        height: getProportionateScreenHeight(40),
+      ),
+    );
+  }
+}
