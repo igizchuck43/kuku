a4276896ba2d471185df5492aea363de7951830c
Igiz <igizstarkle@gmail.com>
2024-08-01 22:17:36 +0300

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,1 +1,17 @@
+import 'package:Kuku/screens/landing_screen/landing_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'screens/complete_profile/complete_profile_screen.dart';
+import 'screens/otp/otp_screen.dart';
+import 'screens/sign_in/sign_in_screen.dart';
+import 'screens/splash_screen/splash_screen.dart';
+
+
+final Map<String, WidgetBuilder> routes = {
+  LandingScreen.routeName:(context) => const LandingScreen(),
+  SplashScreen.routeName: (context) => const SplashScreen(),
+  SignInScreen.routeName: (context) => const SignInScreen(),
+  CompleteProfileScreen.routeName: (context) => const CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => const OtpScreen(),
+
+};
