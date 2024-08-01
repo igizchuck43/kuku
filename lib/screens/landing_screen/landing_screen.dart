61953733ac1c591bd323ee31fafe5e4cde0e7b42
Igiz <igizstarkle@gmail.com>
2024-08-01 17:02:29 +0300

    initial commit

---
+++ lib/screens/landing_screen/landing_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+
+
+class LandingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtils().init(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/images/kurinda_logo.png',
+              fit: BoxFit.cover,
+            ),
+            IntroWidget()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class IntroWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenWidth(
+            20,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Welcome to Kurinda',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      color: kTextColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Spacer(),
+              ],
+            ),
+            Spacer(),
+            Text(
+              'Instant Help, Just a Tap Away',
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                color: kTextColorAccent,
+              ),
+            ),
+            Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(IntroScreen.routeName);
+              },
+              child: Text('Get Started'),
+            ),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
