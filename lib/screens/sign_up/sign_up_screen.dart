9009b069e43b42aea2aa584cf6c02210c99fadb2
Igiz <igizstarkle@gmail.com>
2024-08-01 22:08:01 +0300

    Sign-up details

---
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,66 @@
+import 'package:Kuku/widgets/social_card.dart';
+import 'package:flutter/material.dart';
+
+
+import '../../constants.dart';
+import 'components/sign_up_form.dart';
+
+class SignUpScreen extends StatelessWidget {
+  static String routeName = "/sign_up";
+
+  const SignUpScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 16),
+                  const Text("Register Account", style: headingStyle),
+                  const Text(
+                    "Complete your details or continue \nwith social media",
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 16),
+                  const SignUpForm(),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SocialCard(
+                        icon: "assets/icons/google-icon.svg",
+                        press: () {},
+                      ),
+                      SocialCard(
+                        icon: "assets/icons/facebook-2.svg",
+                        press: () {},
+                      ),
+                      SocialCard(
+                        icon: "assets/icons/twitter.svg",
+                        press: () {},
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'By continuing your confirm that you agree \nwith our Term and Condition',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
