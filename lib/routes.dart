2eb4538a7a02debe44064ab3fda0069c7d405620
Igiz <igizstarkle@gmail.com>
2024-08-02 20:21:05 +0300

    Update routes file

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,6 +5,7 @@ import 'screens/complete_profile/complete_profile_screen.dart';
 import 'screens/home/home_screen.dart';
 import 'screens/login_success/login_success.dart';
 import 'screens/otp/otp_screen.dart';
+import 'screens/profile/profile_screen.dart';
 import 'screens/sign_in/sign_in_screen.dart';
 import 'screens/splash_screen/splash_screen.dart';
 
@@ -17,6 +18,7 @@ final Map<String, WidgetBuilder> routes = {
   OtpScreen.routeName: (context) => const OtpScreen(),
   LoginSuccessScreen.routeName: (context) => const LoginSuccessScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
-
+  ProfileScreen.routeName: (context) => const ProfileScreen(),
+  
 
 };
