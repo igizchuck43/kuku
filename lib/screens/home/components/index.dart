a2826d7d6c46ea0baa7ff902ae2a1de22c4848b7
Igiz <igizstarkle@gmail.com>
2024-08-03 20:39:32 +0300

    Update index screen

--- lib/screens/home/components/index.dart
+++ lib/screens/home/components/index.dart
@@ -1,2 +1,50 @@
 import 'package:flutter/material.dart';
 
+import '../../../widgets/services.dart';
+
+class Index extends StatefulWidget {
+  const Index({super.key});
+
+  @override
+  State<Index> createState() => _IndexState();
+}
+
+class _IndexState extends State<Index> {
+  List<Service> services = [
+    Service('Cleaning',
+        'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-cleaning-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'),
+    Service('Plumber',
+        'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-plumber-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'),
+    Service('Electrician',
+        'https://img.icons8.com/external-wanicon-flat-wanicon/2x/external-multimeter-car-service-wanicon-flat-wanicon.png'),
+    Service('Painter',
+        'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-painter-male-occupation-avatar-itim2101-flat-itim2101.png'),
+    Service('Carpenter', 'https://img.icons8.com/fluency/2x/drill.png'),
+    Service('Gardener',
+        'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-gardener-male-occupation-avatar-itim2101-flat-itim2101.png'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Dashboard',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_none,
+              color: Colors.grey.shade700,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
