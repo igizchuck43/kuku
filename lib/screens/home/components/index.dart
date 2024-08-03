b833d0be2ba6fe6466b2d6a2a19b8213c210e6b9
Igiz <igizstarkle@gmail.com>
2024-08-03 20:50:42 +0300

    Update Index(Home) screen

--- lib/screens/home/components/index.dart
+++ lib/screens/home/components/index.dart
@@ -42,9 +42,213 @@ class _IndexState extends State<Index> {
               color: Colors.grey.shade700,
               size: 30,
             ),
-          )
+          ),
         ],
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/login');
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://uifaces.co/our-content/donated/NY9hnAbp.jpg'),
+              ),
+            ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+        padding: EdgeInsets.only(left: 20.0, top: 10.0, right: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Recent',
+              style:
+              TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  'View all',
+                ),
+              ),
+             ],
+            ),
+           ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                height: 180,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      offset: Offset(0, 4),
+                      blurRadius: 10.0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Image.network(
+                              'https://images.pexels.com/photos/355164/pexels-photo-355164.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb',
+                              width: 70,
+                            )),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Isabel Kirkland",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Cleaner",
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(0.7),
+                                  fontSize: 18),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(15.0)),
+                      child: Center(
+                          child: Text(
+                            'View Profile',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'View all',
+                      ))
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              height: 300,
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.0,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                  ),
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: services.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return FadeAnimation(
+                        (1.0 + index) / 4,
+                        serviceContainer(services[index].imageURL,
+                            services[index].name, index));
+                  }),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Top Rated',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'View all',
+                      ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 150,
+            ),
+          ],
+        ),
       ),
     );
+
+
+
   }
+}
+
+serviceContainer(String image, String name, int index) {
+  return GestureDetector(
+    child: Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade100,
+        border: Border.all(
+          color: Colors.blue.withOpacity(0),
+          width: 2.0,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(image, height: 45),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              name,
+              style: TextStyle(fontSize: 15),
+            )
+          ]),
+    ),
+  );
 }
