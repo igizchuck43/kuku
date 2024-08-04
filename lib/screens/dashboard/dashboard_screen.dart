2ffe3627c68d4d66ca3571a4974b60be1ad88918
Igiz <igizstarkle@gmail.com>
2024-08-04 10:07:24 +0300

    Dashboard screen details

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,14 +1,7 @@
-import 'package:Kuku/screens/home/components/home_banner.dart';
-import 'package:Kuku/screens/home/components/home_header.dart';
 import 'package:flutter/material.dart';
 
-import 'components/index.dart';
-
-
-class HomeScreen extends StatelessWidget {
-  static String routeName = "/home";
-
-  const HomeScreen({super.key});
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +11,11 @@ class HomeScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(vertical: 16),
           child: Column(
             children: [
-              Index(),
-
-
-
+              
             ],
           ),
         ),
       ),
-    );
+    );;
   }
 }
