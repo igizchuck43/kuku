10ccbaad3cf188bc7e1f41e0d7b7239eca29ead6
Igiz <igizstarkle@gmail.com>
2024-08-09 12:32:54 +0300

    Updated SOS screen

--- lib/screens/sos/sos_screen.dart
+++ lib/screens/sos/sos_screen.dart
@@ -23,7 +23,53 @@ class SosScreen extends StatelessWidget {
             height: 16.0,
           ),
         //   Category Field with Dropdown
-          
+          DropdownButtonFormField(
+            decoration: InputDecoration(
+              labelText: "Catergory",
+              prefixIcon: Icon(Icons.category),
+              border: OutlineInputBorder(),
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'Emergency',
+                child: Text("Emergency"),
+              ),
+              DropdownMenuItem(
+                value: 'Official',
+                child: Text("Official"),
+              ),
+            ],
+            onChanged: (value) {
+
+            },
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+        //   Message Field
+          TextField(
+            maxLines: 4,
+            decoration: InputDecoration(
+              labelText: "Add Message",
+              hintText: "Urgent message",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+        //   SOS Button
+          Center(
+            child: ElevatedButton(
+              onPressed: (){
+
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: Text("Send SOS"),
+            ),
+          )
         ],
       ),
     );
