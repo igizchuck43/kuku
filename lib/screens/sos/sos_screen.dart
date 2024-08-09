243f6bbac10e6085439a9bb09d9db4337c3039e4
Igiz <igizstarkle@gmail.com>
2024-08-09 13:22:21 +0300

    Added header

--- lib/screens/sos/sos_screen.dart
+++ lib/screens/sos/sos_screen.dart
@@ -6,72 +6,112 @@ class SosScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        //   Location Field
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Location",
-              hintText: "Nakawa",
-              prefixIcon: Icon(Icons.location_on),
-              border: OutlineInputBorder(),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SOS',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
           ),
-          SizedBox(
-            height: 16.0,
-          ),
-        //   Category Field with Dropdown
-          DropdownButtonFormField(
-            decoration: InputDecoration(
-              labelText: "Catergory",
-              prefixIcon: Icon(Icons.category),
-              border: OutlineInputBorder(),
-            ),
-            items: [
-              DropdownMenuItem(
-                value: 'Emergency',
-                child: Text("Emergency"),
+        ),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 16,
               ),
-              DropdownMenuItem(
-                value: 'Official',
-                child: Text("Official"),
+              decoration: BoxDecoration(
+                color: const Color(0xFF979797),
+                borderRadius: BorderRadius.circular(20),
               ),
-            ],
-            onChanged: (value) {
-
-            },
-          ),
-          SizedBox(
-            height: 16.0,
-          ),
-        //   Message Field
-          TextField(
-            maxLines: 4,
-            decoration: InputDecoration(
-              labelText: "Add Message",
-              hintText: "Urgent message",
-              border: OutlineInputBorder(),
+              child: Text(
+                "Set up alerts to notify past users and emergency contacts when you're in a danger situtation.",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 24.0,
-          ),
-        //   SOS Button
-          Center(
-            child: ElevatedButton(
-              onPressed: (){
+            SizedBox(
+              height: 20,
+            ),
+          //   Location Field
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Location",
+                hintText: "Nakawa",
+                prefixIcon: Icon(Icons.location_on),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+          //   Category Field with Dropdown
+            DropdownButtonFormField(
+              decoration: InputDecoration(
+                labelText: "Catergory",
+                prefixIcon: Icon(Icons.category),
+                border: OutlineInputBorder(),
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'Emergency',
+                  child: Text("Emergency"),
+                ),
+                DropdownMenuItem(
+                  value: 'Official',
+                  child: Text("Official"),
+                ),
+              ],
+              onChanged: (value) {
 
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              child: Text("Send SOS"),
             ),
-          )
-        ],
+            SizedBox(
+              height: 16.0,
+            ),
+          //   Message Field
+            TextField(
+              maxLines: 4,
+              decoration: InputDecoration(
+                labelText: "Add Message",
+                hintText: "Urgent message",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+          //   SOS Button
+            Center(
+              child: ElevatedButton(
+                onPressed: (){
+
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                child: Text("Send SOS",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
